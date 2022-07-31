69a9f9ff7546ced0b1ac28ced4ca5a792a659de0
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 19:44:50 -0500

    Create NumerosDel.cpp

---
+++ NumerosDel.cpp
@@ -0,0 +1,13 @@
+package ExamenParcial2;
+import java.util.*;
+
+public class NumDel1al10 {
+	public static void main(String[] args) {                                                                      
+        int i = 1;
+        do {   
+            System.out.print(i + "_");
+            i++;
+        } while (i<=10);                                                                   
+        System.out.println("\nFin programa");
+    }
+}
